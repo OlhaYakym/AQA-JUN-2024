19b5f4ca55cb37a77cf5689d4fd1a5de3ce862ac
OlhaYakym <olhayakym@gmail.com>
2024-07-03 05:43:15 +0200

    Lesson 1: HW

--- src/main/java/org/prog/Car.java
+++ src/main/java/org/prog/Car.java
@@ -3,15 +3,15 @@ package org.prog;
 public class Car {
 
     public String color;
-    public int milage = 0;
+    public int mileage = 0;
     public int sum = 0;
 
     // TODO: add parameter that indicates distance car travels
     public void goTo(int distance) {
-        milage += 10;
+        mileage += 10;
         sum +=distance;
         System.out.println(color + " car goes somewhere!");
-        System.out.println(color + " car current milage is now " + milage);
+        System.out.println(color + " car current milage is now " + mileage);
         System.out.println(color + " car has traveled a total of miles - " + sum);
     }
 }
