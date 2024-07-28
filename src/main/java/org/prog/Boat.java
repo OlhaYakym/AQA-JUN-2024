bd85dc1c7be66da1ae5efb9a7ff53b4c665ffd56
OlhaYakym <olhayakym@gmail.com>
2024-07-29 00:00:29 +0200

    Lesson 3: HW_3.1

--- src/main/java/org/prog/Boat.java
+++ src/main/java/org/prog/Boat.java
@@ -1,20 +1,12 @@
 package org.prog;
 
 public class Boat implements ITransport {
-
-    @Override
     public void goTo() {
         System.out.println("Boat sails somewhere");
     }
 
-    @Override
     public void turn(String direction) {
         System.out.println("Boat turns " + direction);
-    }
-
-    @Override
-    public void stopAt(String stop) {
-        System.out.println("Boat stops at " + stop);
     }
 
     public void setSail() {
